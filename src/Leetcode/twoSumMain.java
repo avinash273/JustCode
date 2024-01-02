244523da74b1017412e1aa18b5cf8c135a1794e5
avinash273 <savinash273@gmail.com>
2024-01-01 23:41:27 -0600

    Initial commit

---
+++ src/Leetcode/twoSumMain.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+import java.util.HashMap;
+
+public class twoSumMain {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(!map.containsKey(target - nums[i])){
+                map.put(nums[i], i);
+            }
+            else {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+        }
+        return new int[2];
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import Leetcode.twoSumMain;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /**
+         * Main function used to call all program
+         * -Avinash Shanker
+         */
+
+        System.out.println("Main function used to call all program \n");
+
+        /**
+         * https://leetcode.com/problems/two-sum/
+         * Input: nums = [2,7,11,15], target = 9
+         * Output: [0,1]
+         * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+         */
+        twoSumMain twoSumVar = new twoSumMain();
+        int[] output = twoSumVar.twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println("1. twoSum: [" + output[0] + "," + output[1] + "]");
+
+    }
+}
