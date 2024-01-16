313b41719b76b408a24f89ce3f4140c584b723e2
avinash273 <savinash273@gmail.com>
2024-01-15 20:15:33 -0600

    fizzBuzz412

---
+++ src/Leetcode/fizzBuzz412.java
@@ -0,0 +1,27 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class fizzBuzz412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+
+        if (n == 0)
+            return answer;
+
+        for (int i = 1; i <= n; i++) {
+            if ((i % 3) == 0 && (i % 5) == 0) {
+                answer.add("FizzBuzz");
+            } else if ((i % 3) == 0) {
+                answer.add("Fizz");
+            } else if ((i % 5) == 0) {
+                answer.add("Buzz");
+            }
+            else{
+                answer.add(Integer.toString(i));
+            }
+        }
+        return answer;
+    }
+}
